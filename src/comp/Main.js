28ce1f4ef7cf9c03449538fc222893d7ef52acb6
Shaheed <9381heed@gmail.com>
2023-10-07 15:29:46 +0530

    vv

---
+++ src/comp/Main.js
@@ -0,0 +1,27 @@
+import React from 'react'
+export default function Main() {
+	return (
+		<div>
+			this is main contant
+		</div>
+	)
+}
+
+export function Home() {
+	return (
+		<div>this is home content</div>
+	)
+}
+
+export function About() {
+	return (
+		<div>The About Us page of your website is an essential source of information for all who want to know more about your business. About Us pages are where you showcase your history, what is unique about your work, your company's values, and who you serve</div>
+	)
+}
+
+
+export function service() {
+	return (
+		<div>Service Content means the music, images, text and other items that you may obtain or access in digital form from the Service, whose content is owned by the Company or third party licensors.</div>
+	)
+}
